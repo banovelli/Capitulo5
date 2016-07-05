36e07918e9ea9c1373e5131eabac80cabc4e081e
banovelli <bnovelli@gmail.com>
2016-07-05 10:07:43 -0300

    funciona com bugs

--- DinnerParty/DinnerParty/Form1.Designer.cs
+++ DinnerParty/DinnerParty/Form1.Designer.cs
@@ -29,39 +29,52 @@
         private void InitializeComponent()
         {
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.fancyBox = new System.Windows.Forms.CheckBox();
+            this.healthyBox = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.costLbl = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(13, 13);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
-            // checkBox1
+            // fancyBox
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(13, 40);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(114, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "Decoração chique";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.fancyBox.AutoSize = true;
+            this.fancyBox.Location = new System.Drawing.Point(13, 40);
+            this.fancyBox.Name = "fancyBox";
+            this.fancyBox.Size = new System.Drawing.Size(114, 17);
+            this.fancyBox.TabIndex = 1;
+            this.fancyBox.Text = "Decoração chique";
+            this.fancyBox.UseVisualStyleBackColor = true;
+            this.fancyBox.CheckedChanged += new System.EventHandler(this.fancyBox_CheckedChanged);
             // 
-            // checkBox2
+            // healthyBox
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(13, 64);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(104, 17);
-            this.checkBox2.TabIndex = 2;
-            this.checkBox2.Text = "Opção saudável";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.healthyBox.AutoSize = true;
+            this.healthyBox.Location = new System.Drawing.Point(13, 64);
+            this.healthyBox.Name = "healthyBox";
+            this.healthyBox.Size = new System.Drawing.Size(104, 17);
+            this.healthyBox.TabIndex = 2;
+            this.healthyBox.Text = "Opção saudável";
+            this.healthyBox.UseVisualStyleBackColor = true;
+            this.healthyBox.CheckedChanged += new System.EventHandler(this.healthyBox_CheckedChanged);
             // 
             // label1
             // 
@@ -71,27 +84,26 @@
             this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Custo";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // costLbl
             // 
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(54, 88);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 15);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "15";
+            this.costLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.costLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.costLbl.Location = new System.Drawing.Point(54, 88);
+            this.costLbl.Name = "costLbl";
+            this.costLbl.Size = new System.Drawing.Size(79, 15);
+            this.costLbl.TabIndex = 4;
+            this.costLbl.Text = "15";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(144, 109);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.costLbl);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.healthyBox);
+            this.Controls.Add(this.fancyBox);
             this.Controls.Add(this.numericUpDown1);
             this.Name = "Form1";
             this.Text = "Planejador de Festas";
@@ -104,10 +116,10 @@
         #endregion
 
         private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox fancyBox;
+        private System.Windows.Forms.CheckBox healthyBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label costLbl;
     }
 }
 
--- DinnerParty/DinnerParty/Form1.cs
+++ DinnerParty/DinnerParty/Form1.cs
@@ -12,13 +12,39 @@ namespace DinnerParty
 {
     public partial class Form1 : Form
     {
+        DinnerParty dinnerParty;
+
         public Form1()
         {
             InitializeComponent();
+            dinnerParty = new DinnerParty() { NumberofPeople = 5 };
+            dinnerParty.CalculateCostOfDecorations(fancyBox.Checked);
+            dinnerParty.setHeathyOption(healthyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+        
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.CalculateCostOfDecorations(fancyBox.Checked);
+            DisplayDinnerPartyCost();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void healthyBox_CheckedChanged(object sender, EventArgs e)
         {
+            dinnerParty.setHeathyOption(healthyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerParty.NumberofPeople = (int)numericUpDown1.Value;
+            DisplayDinnerPartyCost();
+        }
+
+        private void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerParty.CalculateCost(healthyBox.Checked);
+            costLbl.Text = Cost.ToString("c");
 
         }
     }
